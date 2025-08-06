32dca3a7c447e0faaba0aad7cd287e14d0629bff
Thinuka Vinjaya Wickramanayaka <thinukavinjayauni@gmail.com>
2025-08-06 12:55:33 +0530

    product page make

--- Thread-frontend/src/components/Product/ProductCard.jsx
+++ Thread-frontend/src/components/Product/ProductCard.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import "./ProductCard.css"
 
-const ProductCard = () => {
+const ProductCard = ({product}) => {
   return (
     <div className='productCard w-[15rem] m-3 transition-all cursor-pointer'>
       <div className='h-[20rem]'>
-        <img className='h-full w-full object-cover object-left-top' src='https://rukminim1.flixcart.com/image/612/612/l2dmky80/t-shirt/1/v/f/l-t-shirt-leriya-fashion-original-imagdqkb5gbmew6g.jpeg?q=70' alt=''/>
+        <img className='h-full w-full object-cover object-left-top' src={product.imageUrl} alt=''/>
+        
+      </div>
+      <div className='textPart bg-white p-3'>
+        <div>
+          <p className="font-bold opacity-60">{product.brand}</p>
+          <p>{product.title}</p>
+        </div>
+        <div className='flex items-center space-x-2'>
+            <p className="font-semibold">LKR{product.discountPrice}</p>
+            <p className="line-through opacity-50">{product.price}</p>
+            <p className="text-green-600 font-semibold">{product.discountPersent}% off</p>
+        </div>
       </div>
     </div>
   )
